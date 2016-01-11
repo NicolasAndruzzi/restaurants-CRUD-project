c264b9240d3c7e262c4fa05a671e1e9033857b00
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2016-01-11 10:07:31 -0700

    routes described

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex') ({
+  client: 'pg',
+  connection: 'postgress://localhost/businesses'
+});
+
+function restaurants(){
+  return knex('restaurants');
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/restaurants', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/restaurants/new', function(req, res, next) {
+  res.render('new', { title: 'Express' });
+});
+
+router.get('/restaurants/:name', function(req, res, next) {
+  res.render('show', { title: 'Express' });
+});
+
+router.get('/restaurants/:name/edit', function(req, res, next) {
+  res.render('edit', { title: 'Express' });
+});
+
+console.log(restaurants());
 module.exports = router;
