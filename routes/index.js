467d859fb73ff4b62d53904f95a15c955ca4b123
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2016-01-20 16:58:44 -0700

    routes changed, file structure updated

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('knex') ({
-  client: 'pg',
-  connection: 'postgress://localhost/businesses'
-});
-
-function restaurants(){
-  return knex('restaurants');
-}
 
 /* GET home page. */
-router.get('/restaurants', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.redirect('/restaurants');
 });
 
-router.get('/restaurants/new', function(req, res, next) {
-  res.render('new', { title: 'Express' });
-});
-
-router.get('/restaurants/:name', function(req, res, next) {
-  res.render('show', { title: 'Express' });
-});
-
-router.get('/restaurants/:name/edit', function(req, res, next) {
-  res.render('edit', { title: 'Express' });
-});
-
-console.log(restaurants());
 module.exports = router;
---
+++ routes/restaurants.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('knex') ({
+  client: 'pg',
+  connection: 'postgress://localhost/businesses'
+});
+
+// function restaurants(){
+//   return knex('restaurants');
+// }
+
+/* GET users listing. */
+router.get('/restaurants', function(req, res, next) {
+  res.render('./restaurants/index', {title: "red"});
+});
+
+// console.log(knex('restaurants'));
+module.exports = router;
