fc985e19e8cb925779c3c819f1b03ce6aaa2738a
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2016-02-02 03:27:58 -0700

    CRUD completely working for restaurants

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -22,7 +22,7 @@ router.get('/new', function(req, res, next) {
 });
 
 router.post('/new', function(req, res, next) {
-  restaurants().insert({name: "Hello"}).then(function(results) {
+  restaurants().insert({name: req.body.name, city: req.body.city, state: req.body.state, cuisine: req.body.cuisine, rating: req.body.rating, img: req.body.imgURL, description: req.body.description}).then(function(results) {
     res.redirect('/restaurants');
   });
 });
@@ -40,19 +40,38 @@ router.get('/:id/edit', function(req, res, next) {
 });
 
 router.post('/:id/edit', function(req, res, next) {
-    var updateItems = {
-      name: req.body.name,
-      city: req.body.city
-    }
+    var updateItems = {name: req.body.name, city: req.body.city, state: req.body.state, cuisine: req.body.cuisine, rating: req.body.rating, img: req.body.imgURL, description: req.body.description}
     restaurants().update(updateItems).where('id', req.params.id).then(function(results){
-        res.redirect('/restaurants/'+req.params.id);
-      })
+      res.redirect('/restaurants/'+req.params.id);
+    });
 });
 
 router.get('/:id/delete', function(req, res, next) {
   restaurants().select().where("id", req.params.id).del().then(function(results){
     res.redirect('/restaurants');
   });
+});
+
+router.get('/:id/map', function(req,res,next){
+
+      var google_api = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+      var address = 'Zuni+St+&+W+34th+Ave,+Denver,+CO+80211'
+      var my_key = "&key=AIzaSyBlnwQZvWxJEBUc7mJETX1B4cKrdYEjh30";
+
+
+  request(google_api+address+my_key, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var jase = JSON.parse(body);
+      var j = jase.results[0];
+      var lat_long = j.geometry.location
+      var lat = Number(lat_long.lat)
+      var lng = Number(lat_long.lng)
+
+
+
+      res.render('map', { header: 'Reviews', lat: lat, long: lng});
+    }
+  })
 });
 
 
